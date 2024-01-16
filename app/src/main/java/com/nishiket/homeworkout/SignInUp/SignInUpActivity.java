a9540db3761efed504e6e6e0c2cc3a1b4019c750
nishiket04 <nishiket04@gmail.com>
2024-01-16 10:18:23 +0530

    signin page done

--- app/src/main/java/com/nishiket/homeworkout/SignInUp/SignInUpActivity.java
+++ app/src/main/java/com/nishiket/homeworkout/SignInUp/SignInUpActivity.java
@@ -1,8 +1,12 @@
 package com.nishiket.homeworkout.SignInUp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Fragment;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 import com.nishiket.homeworkout.R;
 
@@ -12,5 +16,9 @@ public class SignInUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_up);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        ft.add(R.id.frame,new SignInFragment()).commit();
+
     }
 }
