95c7f636a1f71fd9b7a9b07386f2f8db4d6691bd
nishiket04 <nishiket04@gmail.com>
2024-01-17 14:31:42 +0530

    made only email login with otp verificarion

--- app/src/main/java/com/nishiket/homeworkout/SignInUp/VerificationFragment.java
+++ app/src/main/java/com/nishiket/homeworkout/SignInUp/VerificationFragment.java
@@ -27,54 +27,19 @@ import com.nishiket.homeworkout.R;
 
 import java.util.concurrent.TimeUnit;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link VerificationFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class VerificationFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public VerificationFragment() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment VerifiactionFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static VerificationFragment newInstance(String param1, String param2) {
-        VerificationFragment fragment = new VerificationFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
-    String otp;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -83,6 +48,7 @@ public class VerificationFragment extends Fragment {
     }
 
     private String verificationId;
+    private String otp;
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -249,26 +215,13 @@ public class VerificationFragment extends Fragment {
                     @Override
                     public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
                         super.onCodeSent(s, forceResendingToken);
-                       String verificationId = s;
+                       verificationId = s;
                     }
                 });
     }
     private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-        firebaseAuth.signInWithCredential(credential)
-                .addOnCompleteListener(getActivity(), new OnCompleteListener() {
-                    @Override
-                    public void onComplete(@NonNull Task task) {
-                        if (task.isSuccessful()) {
-                            // Phone number verified successfully
-                            createAccountWithEmailPassword(); // Create account with email/password
-                        } else {
-                            // Verification failed
-                            Log.w("done", "signInWithCredential:failure", task.getException());
-                            Toast.makeText(getActivity(), "Verification failed", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+        createAccountWithEmailPassword();
     }
 
     private void createAccountWithEmailPassword() {
