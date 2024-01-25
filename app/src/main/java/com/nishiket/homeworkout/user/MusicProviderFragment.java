f1f826088da05d90e155672435b09d10630e1375
nishiket04 <nishiket04@gmail.com>
2024-01-25 14:33:43 +0530

    Music Provider frontend

---
+++ app/src/main/java/com/nishiket/homeworkout/user/MusicProviderFragment.java
@@ -0,0 +1,25 @@
+package com.nishiket.homeworkout.user;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nishiket.homeworkout.R;
+
+public class MusicProviderFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_music_provider, container, false);
+    }
+}
