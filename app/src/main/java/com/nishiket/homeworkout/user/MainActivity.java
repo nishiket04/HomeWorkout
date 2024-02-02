906275ecd54d583f8cb71060c1b4a8edaa3fd3a9
nishiket04 <nishiket04@gmail.com>
2024-02-02 11:51:23 +0530

    bottom nav item select fix

--- app/src/main/java/com/nishiket/homeworkout/user/MainActivity.java
+++ app/src/main/java/com/nishiket/homeworkout/user/MainActivity.java
@@ -31,6 +31,8 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         bottom_navigation_view.setOnNavigationItemSelectedListener(this);
         bottom_navigation_view.setSelectedItemId(R.id.home);
 
+        bottom_navigation_view.getMenu().findItem(R.id.home).setIcon(R.drawable.home);
+
     }
 
     @SuppressLint("NonConstantResourceId")
@@ -42,24 +44,40 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
                         .beginTransaction()
                         .replace(R.id.homeFrameLayout, new HomeFragment())
                         .commit();
+                item.setIcon(R.drawable.home_selected);
+                bottom_navigation_view.getMenu().findItem(R.id.traing).setIcon(R.drawable.training);
+                bottom_navigation_view.getMenu().findItem(R.id.activity).setIcon(R.drawable.activity);
+                bottom_navigation_view.getMenu().findItem(R.id.user).setIcon(R.drawable.user);
                 return true;
             } else if (item.getItemId() == R.id.traing) {
                 getSupportFragmentManager()
                         .beginTransaction()
                         .replace(R.id.homeFrameLayout, new TrainingsFragment())
                         .commit();
+                item.setIcon(R.drawable.trainign_selected);
+                bottom_navigation_view.getMenu().findItem(R.id.home).setIcon(R.drawable.home);
+                bottom_navigation_view.getMenu().findItem(R.id.activity).setIcon(R.drawable.activity);
+                bottom_navigation_view.getMenu().findItem(R.id.user).setIcon(R.drawable.user);
                 return true;
             } else if (item.getItemId()==R.id.activity) {
                 getSupportFragmentManager()
                         .beginTransaction()
                         .replace(R.id.homeFrameLayout, new ActivityFragment())
                         .commit();
+                item.setIcon(R.drawable.activity_selected);
+                bottom_navigation_view.getMenu().findItem(R.id.user).setIcon(R.drawable.user);
+                bottom_navigation_view.getMenu().findItem(R.id.traing).setIcon(R.drawable.training);
+                bottom_navigation_view.getMenu().findItem(R.id.home).setIcon(R.drawable.home);
                 return true;
             } else if (item.getItemId() == R.id.user) {
                 getSupportFragmentManager()
                         .beginTransaction()
                         .replace(R.id.homeFrameLayout, new ProfileFragment())
                         .commit();
+                item.setIcon(R.drawable.profile_selected);
+                bottom_navigation_view.getMenu().findItem(R.id.traing).setIcon(R.drawable.training);
+                bottom_navigation_view.getMenu().findItem(R.id.activity).setIcon(R.drawable.activity);
+                bottom_navigation_view.getMenu().findItem(R.id.home).setIcon(R.drawable.home);
                 return true;
             }
 
