8daa71705e9b48f300e4d4af9c4b0766f5fd90e6
nishiket04 <nishiket04@gmail.com>
2024-02-02 16:43:36 +0530

    my workout back and checkBox

--- app/src/main/java/com/nishiket/homeworkout/user/MyWorkoutFragment.java
+++ app/src/main/java/com/nishiket/homeworkout/user/MyWorkoutFragment.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.google.android.material.tabs.TabLayout;
 import com.nishiket.homeworkout.R;
@@ -31,6 +33,7 @@ public class MyWorkoutFragment extends Fragment {
 
     private TabLayout myWorkoutTabLayout;
     private ViewPager myWorkoutViewPager;
+    private ImageView backToProfile;
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -40,10 +43,19 @@ public class MyWorkoutFragment extends Fragment {
         MyWorkoutViewPagerAdapter myWorkoutViewPagerAdapter = new MyWorkoutViewPagerAdapter(getParentFragmentManager());
         myWorkoutViewPager.setAdapter(myWorkoutViewPagerAdapter);
         myWorkoutTabLayout.setupWithViewPager(myWorkoutViewPager);
+
+        backToProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getParentFragmentManager();
+                fragmentManager.popBackStack();
+            }
+        });
     }
 
     private void assignId(View view) {
         myWorkoutTabLayout = view.findViewById(R.id.myWorkoutTabLayout);
         myWorkoutViewPager = view.findViewById(R.id.myWorkoutViewPager);
+        backToProfile = view.findViewById(R.id.backToProfileImage);
     }
 }
